f26048e4cd1bfd57988539b88a831afbc38c9e94
MargoShulpina <MargoShulpina@users.noreply.github.com>
2018-09-22 11:34:24 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let num = equation.replace(" * x^2 ",",")
+        let num2 = num.replace(" * x",",")
+        let num3 = num2.replace(/\ /g,"")
+        var arr = num3.split(",")
+        var a = Number(arr[0])
+        var b = Number(arr[1])
+        var c = Number(arr[2])
+        var d = (- b - Math.sqrt(b * b - 4 * a * c))/(2 * a)
+        var e = (- b + Math.sqrt(b * b - 4 * a * c))/(2 * a)
+        
+       if (d<e){
+        return [Math.round(d), Math.round(e)];}
+        else{
+         return [Math.round(e), Math.round(d)]}
+      
+    
+
 }
